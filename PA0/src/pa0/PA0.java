4b5ef7a754239595f3a02511a8379a16912e16e5
Alberto Sierra <alberto.sierra@murciaeduca.es>
2019-03-27 09:31:34 +0100

    Estructura PA0

---
+++ PA0/src/pa0/PA0.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pa0;
+
+/**
+ *
+ * @author Alberto
+ */
+public class PA0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean finalizar;
+        do {
+            String entrada = leerEntrada();
+            if(!(finalizar = finalDeJuego())) {
+                logica();
+                escribirSalida();
+            }
+        }
+        while(!finalizar);
+    }
+
+    // Jugador1
+    private static String leerEntrada() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    //Jugador2
+    private static boolean finalDeJuego() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void logica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void escribirSalida() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
